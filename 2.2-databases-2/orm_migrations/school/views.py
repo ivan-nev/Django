f75e77fc20d9cf499de0401d8c97bafeb5228413
ivan-nev <i_nevakshenov@mail.ru>
2022-11-09 20:08:48 +0500

    add  students = Student.objects.prefetch_related("teachers").all()

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -11,11 +11,12 @@ def students_list(request):
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
-    students = Student.objects.all().prefetch_related("students")
+    students = Student.objects.prefetch_related("teachers").all()
     students1 = Student.objects.all().order_by(ordering)
     # for student in students:
-    #     print(student)
-    context = {'object_list':students1}
+    #     for techer in student.teachers.all():
+    #         print(f'stud: {student}, ucit{techer}')
+    context = {'object_list':students}
     print(context)
         # filter(positions__product__price__lte=600)
 
