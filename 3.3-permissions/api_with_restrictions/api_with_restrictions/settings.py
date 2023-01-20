34bbe809e72cbbdaffc8de0a08feb1fb1d602aeb
ivan-nev <i_nevakshenov@mail.ru>
2023-01-20 23:19:49 +0500

    Добавлен поиск складов, где есть определенный продукт по имени

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'z5^+-9&b+ok(d%hl!%lbft$$bd(yz$3g5cq_ch6pw5l#8+ap+-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
